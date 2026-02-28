2fe2814b6b3f592b2a194520e3811ed5b60a3f47
saijaku0 <minerking112256@gmail.com>
2026-02-28 18:28:06 +0100

    feat: add injection registration mediatr service

---
+++ src/Sneakers.Shop.Backend.Application/Injection/MediatRInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Sneakers.Shop.Backend.Application.Injection
+{
+    public static class MediatRInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg =>
+                cfg.RegisterServicesFromAssembly(typeof(MediatRInjection).Assembly));
+            return services;
+        }
+    }
+}
