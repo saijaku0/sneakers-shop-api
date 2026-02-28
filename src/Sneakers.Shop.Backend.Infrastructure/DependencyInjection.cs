3234348bbf817d6d8fcda0475aeedc5c00162e92
saijaku0 <minerking112256@gmail.com>
2026-02-28 18:30:47 +0100

    feat: add service to injection

---
+++ src/Sneakers.Shop.Backend.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sneakers.Shop.Backend.Infrastructure.Persistence;
+
+namespace Sneakers.Shop.Backend.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection service, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            service.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(connectionString));
+            return service;
+        }
+    }
+}
