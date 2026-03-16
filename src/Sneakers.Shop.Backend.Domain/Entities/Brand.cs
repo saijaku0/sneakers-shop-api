ae57733a9e679c4b0a125733040646097eeddf92
saijaku0 <minerking112256@gmail.com>
2026-03-16 13:19:23 +0100

    feat: add DDD logic to brand enitite

--- src/Sneakers.Shop.Backend.Domain/Entities/Brand.cs
+++ src/Sneakers.Shop.Backend.Domain/Entities/Brand.cs
@@ -1,8 +1,26 @@
-﻿namespace Sneakers.Shop.Backend.Domain.Entities
+﻿using Sneakers.Shop.Backend.Domain.Exceptions;
+
+namespace Sneakers.Shop.Backend.Domain.Entities
 {
     public class Brand
     {
         public Guid Id { get; private set; }
         public string BrandName { get; private set; } = string.Empty;
+
+        public Brand() { }
+
+        public Brand(string newName)
+        {
+            UpdateBrandName(newName);
+            Id = Guid.NewGuid();
+        }
+
+        public void UpdateBrandName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new DomainException("New brand name cannot be empty");
+
+            BrandName = name;
+        }
     }
 }
