fd7394605868d2d458ff23bdd6c8dc936822610c
saijaku0 <minerking112256@gmail.com>
2026-03-16 13:29:48 +0100

    feat: add soft delete method

--- src/Sneakers.Shop.Backend.Domain/Entities/Brand.cs
+++ src/Sneakers.Shop.Backend.Domain/Entities/Brand.cs
@@ -6,21 +6,32 @@ namespace Sneakers.Shop.Backend.Domain.Entities
     {
         public Guid Id { get; private set; }
         public string BrandName { get; private set; } = string.Empty;
+        public bool IsDeleted { get; private set; }
+        public DateTimeOffset? DeletedAt { get; private set; }
 
-        public Brand() { }
+        private Brand() { }
 
         public Brand(string newName)
         {
+            IsDeleted = false;
+
             UpdateBrandName(newName);
             Id = Guid.NewGuid();
         }
 
         public void UpdateBrandName(string name)
         {
+            if (IsDeleted) throw new DomainException("You cannot change removed brand name");
             if (string.IsNullOrWhiteSpace(name))
                 throw new DomainException("New brand name cannot be empty");
 
             BrandName = name;
+        }
+
+        public void DeleteBrand()
+        {
+            IsDeleted = true;
+            DeletedAt = DateTimeOffset.UtcNow;
         }
     }
 }
