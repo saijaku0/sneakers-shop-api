8d01cb179b53ef5fff2a7167c6784743471ebf63
saijaku0 <minerking112256@gmail.com>
2026-03-17 21:13:36 +0100

    feat: add size domain

---
+++ src/Sneakers.Shop.Backend.Domain/Entities/Size.cs
@@ -0,0 +1,27 @@
+﻿using Sneakers.Shop.Backend.Domain.Enums;
+using Sneakers.Shop.Backend.Domain.Exceptions;
+
+namespace Sneakers.Shop.Backend.Domain.Entities
+{
+    public class Size
+    {
+        public Guid Id { get; private set; }
+        public MeasureSizes MeasurementType { get; private set; }
+        public decimal Value { get; private set; }
+
+        private Size() { }
+
+        public Size(
+            MeasureSizes measureSizes,
+            decimal val)
+        {
+            if (val <= 0)
+                throw new DomainException($"Cannot add {val} less than or equal 0");
+
+            Id = Guid.NewGuid();
+            MeasurementType = measureSizes;
+            Value = val;
+        }
+
+    }
+}
