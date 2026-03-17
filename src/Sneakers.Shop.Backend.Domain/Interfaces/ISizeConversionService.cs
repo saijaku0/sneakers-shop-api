e7a87c5aaec2b303f7f0479d2712705845b4b94f
saijaku0 <minerking112256@gmail.com>
2026-03-17 21:22:18 +0100

    feat: add interface for conversion size service

---
+++ src/Sneakers.Shop.Backend.Domain/Interfaces/ISizeConversionService.cs
@@ -0,0 +1,10 @@
+﻿
+using Sneakers.Shop.Backend.Domain.Enums;
+
+namespace Sneakers.Shop.Backend.Domain.Interfaces
+{
+    public interface ISizeConversionService
+    {
+        decimal GetEquivalentSize(decimal size, MeasureSizes measureType, MeasureSizes targetMeasureType, string audience);
+    }
+}
