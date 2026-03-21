2535ac52d0ee94dd6a1970a72c078ff067b80d5f
saijaku0 <minerking112256@gmail.com>
2026-03-21 17:25:13 +0100

    refactor: rename discontType to discountType

--- src/Sneakers.Shop.Backend.Domain/Entities/Discount.cs
+++ src/Sneakers.Shop.Backend.Domain/Entities/Discount.cs
@@ -6,7 +6,7 @@ namespace Sneakers.Shop.Backend.Domain.Entities
     public class Discount
     {
         public Guid Id { get; private set; }
-        public DiscontType DiscontType { get; private set; }
+        public DiscountType TypeDiscount { get; private set; }
         public DateTimeOffset StartDate { get; private set; }
         public DateTimeOffset EndDate { get; private set; }
         public decimal DiscountValue { get; private set; }
@@ -17,7 +17,7 @@ namespace Sneakers.Shop.Backend.Domain.Entities
 
         public Discount(
             decimal value,
-            DiscontType discount,
+            DiscountType discount,
             DateTimeOffset start,
             DateTimeOffset end,
             Guid? productId,
@@ -25,16 +25,16 @@ namespace Sneakers.Shop.Backend.Domain.Entities
         {
             if (start > end)
                 throw new DomainException("Cannot create discont which end before start.");
-            if (discount == DiscontType.FixedAmount && value < 0)
+            if (discount == DiscountType.FixedAmount && value < 0)
                 throw new DomainException($"Discont: {value}, cannot be less than zero.");
-            if (discount == DiscontType.Percentage && (value > 100 || value < 0))
+            if (discount == DiscountType.Percentage && (value > 100 || value < 0))
                 throw new DomainException("Percentage discount must be between 0 and 100.", nameof(value));
             if (productId.HasValue == brandId.HasValue)
                 throw new DomainException("Discount must be applied either to a product or to a brand.");
 
             Id = Guid.NewGuid();
             DiscountValue = value;
-            DiscontType = discount;
+            TypeDiscount = discount;
             StartDate = start;
             EndDate = end;
             ProductId = productId;
