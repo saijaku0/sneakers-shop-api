1f33ee4263d3f26a74d254ee57ef2991a5b8977e
saijaku0 <minerking112256@gmail.com>
2026-03-25 18:00:35 +0100

    feat: add configuration: comment discount and promocode

---
+++ src/Sneakers.Shop.Backend.Infrastructure/Configurations/CommentConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sneakers.Shop.Backend.Domain.Entities;
+
+namespace Sneakers.Shop.Backend.Infrastructure.Configurations
+{
+    public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.ToTable($"{nameof(Comment)}s");
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Description)
+                .HasMaxLength(1000);
+
+            builder.Property(c => c.Review)
+                .IsRequired();
+
+            builder.HasOne<Product>()
+                .WithMany(p => p.Comments)
+                .HasForeignKey(c => c.ProductId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne<UserProfile>()
+                .WithMany()
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ src/Sneakers.Shop.Backend.Infrastructure/Configurations/PromoCodeConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sneakers.Shop.Backend.Domain.Entities;
+
+namespace Sneakers.Shop.Backend.Infrastructure.Configurations
+{
+    public class PromoCodeConfiguration : IEntityTypeConfiguration<PromoCode>
+    {
+        public void Configure(EntityTypeBuilder<PromoCode> builder)
+        {
+            builder.ToTable($"{nameof(PromoCode)}s");
+            builder.HasKey(d => d.Id);
+
+            builder.Property(p => p.Code)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            builder.Property(p => p.DiscountValue)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+            builder.Property(p => p.DiscountType)
+                .IsRequired();
+            builder.Property(p => p.ValidFrom)
+                .IsRequired();
+            builder.Property(p => p.ExpirationDate)
+                .IsRequired();
+            builder.Property(p => p.IsUsed)
+                .IsRequired();
+            builder.HasIndex(p => p.Code)
+                .IsUnique();
+
+            builder.HasOne<UserProfile>()
+                .WithMany()                        
+                .HasForeignKey(p => p.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne<Product>()
+                .WithMany()                         
+                .HasForeignKey(p => p.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
