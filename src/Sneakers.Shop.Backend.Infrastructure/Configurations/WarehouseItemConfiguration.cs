af517daefb4dd355d1f92c2c4b8504ee1cce6342
saijaku0 <minerking112256@gmail.com>
2026-03-25 17:30:08 +0100

    feat: add warehouse configuration

---
+++ src/Sneakers.Shop.Backend.Infrastructure/Configurations/WarehouseItemConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sneakers.Shop.Backend.Domain.Entities;
+
+namespace Sneakers.Shop.Backend.Infrastructure.Configurations
+{
+    public class WarehouseItemConfiguration : IEntityTypeConfiguration<WarehouseItem>
+    {
+        public void Configure(EntityTypeBuilder<WarehouseItem> builder)
+        {
+            builder.ToTable($"{nameof(WarehouseItem)}s");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(w => w.Quantity)
+                .IsRequired();
+
+            builder.HasOne<Size>()
+                .WithMany()
+                .HasForeignKey(s => s.SizeId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne<Product>()
+                .WithMany()
+                .HasForeignKey(p => p.ProductId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasIndex(x => new {x.ProductId, x.SizeId})
+                .IsUnique();
+        }
+    }
+}
