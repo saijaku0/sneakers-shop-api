3d562e00f54149ec8b39b021880c98d9995cf071
saijaku0 <minerking112256@gmail.com>
2026-03-25 16:33:59 +0100

    feat: add configuration: targetaudience, size, brand

---
+++ src/Sneakers.Shop.Backend.Infrastructure/Configurations/BrandConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sneakers.Shop.Backend.Domain.Entities;
+
+namespace Sneakers.Shop.Backend.Infrastructure.Configurations
+{
+    public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.ToTable(nameof(Brand));
+            builder.HasKey(b => b.Id);
+
+            builder.Property(n => n.BrandName).IsRequired().HasMaxLength(100);
+        }
+    }
+}
---
+++ src/Sneakers.Shop.Backend.Infrastructure/Configurations/SizeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sneakers.Shop.Backend.Domain.Entities;
+
+namespace Sneakers.Shop.Backend.Infrastructure.Configurations
+{
+    public class SizeConfiguration : IEntityTypeConfiguration<Size>
+    {
+        public void Configure(EntityTypeBuilder<Size> builder)
+        {
+            builder.ToTable(nameof(Size));
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Value).IsRequired().HasColumnType("decimal(4,1)");
+        }
+    }
+}
