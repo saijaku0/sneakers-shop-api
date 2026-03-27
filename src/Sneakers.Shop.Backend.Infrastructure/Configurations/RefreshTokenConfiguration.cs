e8f5889d6164c8fdb0c89998c8716df8475dc69c
saijaku0 <minerking112256@gmail.com>
2026-03-27 21:58:49 +0100

    feat: add configuration for token entity

---
+++ src/Sneakers.Shop.Backend.Infrastructure/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sneakers.Shop.Backend.Domain.Entities;
+
+namespace Sneakers.Shop.Backend.Infrastructure.Configurations
+{
+    public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.ToTable($"{nameof(RefreshToken)}s");
+            builder.HasKey(rt => rt.Id);
+
+            builder.Property(rt => rt.Token)
+                .IsRequired()
+                .HasMaxLength(256);
+            builder.HasIndex(rt => rt.Token)
+                .IsUnique();
+
+            builder.HasOne<UserProfile>()
+                .WithMany()
+                .HasForeignKey(rt => rt.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Property(rt => rt.CreatedAt)
+                .IsRequired();
+            builder.Property(rt => rt.ExpiresAt)
+                .IsRequired();
+            builder.Property(rt => rt.IsRevoked)
+                .IsRequired();
+        }
+    }
+}
