d5dbabf066b316bfeeb1fcd1db5fc7b714bdf06d
saijaku0 <minerking112256@gmail.com>
2026-03-27 19:32:08 +0100

    feat: add contract for refresh token

---
+++ src/Sneakers.Shop.Backend.Domain/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,12 @@
+﻿using Sneakers.Shop.Backend.Domain.Entities;
+
+namespace Sneakers.Shop.Backend.Domain.Interfaces
+{
+    public interface IRefreshTokenRepository
+    {
+        Task AddAsync(RefreshToken token);
+        Task<RefreshToken?> GetByTokenAsync(string token);
+        Task<IReadOnlyList<RefreshToken>> GetActiveTokensForUserAsync(Guid userId);
+        Task RemoveExpiredOrRevokedAsync();
+    }
+}
