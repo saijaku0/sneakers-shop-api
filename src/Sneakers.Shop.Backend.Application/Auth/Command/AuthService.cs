4795966ce838542c3b325f4ac7f4cc70fdec338d
saijaku0 <minerking112256@gmail.com>
2026-03-28 10:19:42 +0100

    feat: add all other method for user login service

--- src/Sneakers.Shop.Backend.Application/Auth/Command/AuthService.cs
+++ src/Sneakers.Shop.Backend.Application/Auth/Command/AuthService.cs
@@ -38,34 +38,81 @@ namespace Sneakers.Shop.Backend.Application.Auth.Command
 
             await _identity.AssignRole(userId, UserRole.Customer, ct);
 
+            return await GenerateNewPairToken(userId, ct);
+        }
+
+        /// <summary>
+        /// Authenticates a user asynchronously using the provided login credentials and returns an authentication
+        /// response containing access tokens.
+        /// </summary>
+        /// <param name="request">The login request containing the user's email and password. Cannot be null.</param>
+        /// <param name="ct">A cancellation token that can be used to cancel the login operation.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains an authentication response with
+        /// access and refresh tokens for the authenticated user.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the user is not found or if the login credentials are invalid.</exception>
+        public async Task<AuthResponse> LoginAsync(
+            LoginRequest request,
+            CancellationToken ct = default)
+        {
+            var userId = await _identity.FindUserByEmailAsync(request.Email, ct)
+                ?? throw new InvalidOperationException($"User is not found. {request.Email}");
+            var password = await _identity.CheckUserPassword(userId, request.Password, ct);
+            if (!password) 
+                throw new InvalidOperationException("Login or password invalid");
+
+            return await GenerateNewPairToken(userId, ct);
+        }
+
+        /// <summary>
+        /// Generates a new authentication token pair using the specified refresh token.
+        /// </summary>
+        /// <remarks>The method revokes the provided refresh token and issues a new token pair. The old
+        /// token cannot be reused after this operation.</remarks>
+        /// <param name="refreshToken">The refresh token used to request a new authentication token pair. Cannot be null or invalid.</param>
+        /// <param name="ct">A cancellation token that can be used to cancel the operation.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains an <see cref="AuthResponse"/>
+        /// with the new authentication token pair.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the specified refresh token is null, not found, or is not valid.</exception>
+        public async Task<AuthResponse> RefreshTokenAsync
+            (string refreshToken, 
+            CancellationToken ct = default)
+        {
+            var getUserToken = await _refreshToken.GetByTokenAsync(refreshToken)
+                ?? throw new InvalidOperationException("Token cannot be null");
+            if (!getUserToken.IsValid())
+                throw new InvalidOperationException("Throw token not valid");
+
+            await _refreshToken.RemoveExpiredOrRevokedAsync();
+            getUserToken.Revoke();
+
+            return await GenerateNewPairToken(getUserToken.UserId, ct);
+        }
+
+        /// <summary>
+        /// Generates a new pair of access and refresh tokens for the specified user.
+        /// </summary>
+        /// <remarks>The generated refresh token is persisted and associated with the specified user.
+        /// Existing refresh tokens are not revoked by this method.</remarks>
+        /// <param name="userId">The unique identifier of the user for whom the tokens are generated.</param>
+        /// <param name="ct">A cancellation token that can be used to cancel the asynchronous operation.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains an AuthResponse with the newly
+        /// generated access and refresh tokens.</returns>
+        private async Task<AuthResponse> GenerateNewPairToken(Guid userId, CancellationToken ct)
+        {
             var tokenData = await _identity.GetTokenGenerationDataAsync(userId, ct);
             var accessToken = _jwtService.GenerateAccessToken(tokenData);
-            var refreshToken = _jwtService.GenerateRefreshToken();
+            var updateToken = _jwtService.GenerateRefreshToken();
 
             var refreshTokenEntity = new RefreshToken(
                 userId: userId,
-                token: refreshToken,
+                token: updateToken,
                 expiresAt: DateTimeOffset.UtcNow.AddDays(_jwtService.GetRefreshTokenExpiryDays())
             );
 
             await _refreshToken.AddAsync(refreshTokenEntity);
             await _unitOfWork.SaveChangesAsync(ct);
 
-            return new AuthResponse(accessToken, refreshToken);
-        }
-
-        public Task<AuthResponse> LoginAsync(
-            LoginRequest request,
-            CancellationToken ct = default)
-        {
-            
-        }
-
-        public Task<AuthResponse> RefreshTokenAsync
-            (string refreshToken, 
-            CancellationToken ct = default)
-        {
-
+            return new AuthResponse(accessToken, updateToken);
         }
     }
 }
