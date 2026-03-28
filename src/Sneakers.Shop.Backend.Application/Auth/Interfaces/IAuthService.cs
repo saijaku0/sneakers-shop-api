80a3722443e89905e14462b1f3c99e533cf7d1ca
saijaku0 <minerking112256@gmail.com>
2026-03-28 09:46:44 +0100

    chore: move interface to nested auth folder

---
+++ src/Sneakers.Shop.Backend.Application/Auth/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Sneakers.Shop.Backend.Application.Auth.DTOs;
+
+namespace Sneakers.Shop.Backend.Application.Auth.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> RegisterAsync(RegisterRequest request, CancellationToken ct = default);
+        Task<AuthResponse> LoginAsync(LoginRequest request, CancellationToken ct = default);
+        Task<AuthResponse> RefreshTokenAsync(string refreshToken, CancellationToken ct = default);
+    }
+}
