5d9ade5376bb97175185559ebce11c7b7224f729
saijaku0 <minerking112256@gmail.com>
2026-04-01 11:02:03 +0200

    feat: add branddiscount for brand inherited dicount

---
+++ src/Sneakers.Shop.Backend.Domain/Entities/BrandDiscount.cs
@@ -0,0 +1,22 @@
+﻿using Sneakers.Shop.Backend.Domain.Enums;
+
+namespace Sneakers.Shop.Backend.Domain.Entities
+{
+    public class BrandDiscount : Discount
+    {
+        public Guid BrandId { get; private set; }
+        protected BrandDiscount() { }
+        public BrandDiscount(
+            Guid brandId,
+            decimal value,
+            DiscountType discount,
+            DateTimeOffset start,
+            DateTimeOffset end) : base(value, discount, start, end)
+        {
+            if (brandId == Guid.Empty)
+                throw new ArgumentException("BrandId cannot be empty.", nameof(brandId));
+
+            BrandId = brandId;
+        }
+    }
+}
