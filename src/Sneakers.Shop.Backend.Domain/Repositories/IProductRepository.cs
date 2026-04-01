01bebbadc5c1f6a3713f3555b80c26617f8e922f
saijaku0 <minerking112256@gmail.com>
2026-04-01 11:03:18 +0200

    fix: dicount repo, update product repo include dicount product

--- src/Sneakers.Shop.Backend.Domain/Repositories/IProductRepository.cs
+++ src/Sneakers.Shop.Backend.Domain/Repositories/IProductRepository.cs
@@ -6,6 +6,7 @@ namespace Sneakers.Shop.Backend.Domain.Repositories
     {
         Task<Product?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
         Task<Product?> GetByIdWithDetailsAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<int> GetTotalProductsCountAsync(CancellationToken cancellationToken);
         Task<IReadOnlyList<Product>> GetAllActiveAsync(int page, int pageSize, CancellationToken cancellationToken = default);
         Task AddAsync(Product product, CancellationToken cancellationToken = default);
         void Update(Product product);
