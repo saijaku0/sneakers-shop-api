9bc5809a9ee52f805e218902625b4cf8f9f4e1a9
saijaku0 <minerking112256@gmail.com>
2026-04-01 10:56:29 +0200

    feat: add product handler respons and query data

---
+++ src/Sneakers.Shop.Backend.Application/Products/Queries/GetProducts/GetProductsResponse.cs
@@ -0,0 +1,6 @@
+﻿using Sneakers.Shop.Backend.Application.Products.DTOs;
+
+namespace Sneakers.Shop.Backend.Application.Products.Queries.GetProducts
+{
+    public record GetProductsResponse(IReadOnlyList<ProductsDto> Products, int TotalCount);
+}
