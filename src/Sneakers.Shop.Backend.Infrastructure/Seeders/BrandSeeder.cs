a8ce9cb23dae972adb4affd177964ea5cfb2fade
saijaku0 <minerking112256@gmail.com>
2026-04-02 22:26:05 +0200

    feat: add brand seeder

---
+++ src/Sneakers.Shop.Backend.Infrastructure/Seeders/BrandSeeder.cs
@@ -0,0 +1,22 @@
+﻿using Sneakers.Shop.Backend.Domain.Entities;
+using Sneakers.Shop.Backend.Domain.Repositories;
+
+namespace Sneakers.Shop.Backend.Infrastructure.Seeders
+{
+    public class BrandSeeder(
+        IBrandRepository brandRepository,
+        IUnitOfWork unitOfWork)
+    {
+        private readonly IBrandRepository _brandRepository = brandRepository;
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+        private static readonly Guid TestCategoryId = Guid.Parse("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa");
+        public async Task SeedAsync()
+        {
+            if (await _brandRepository.GetAllAsync() is not { Count: 0 })
+                return;
+            var brand = new Brand(TestCategoryId, "Nike");
+            await _brandRepository.AddAsync(brand);
+            await _unitOfWork.SaveChangesAsync();
+        }
+    }
+}
