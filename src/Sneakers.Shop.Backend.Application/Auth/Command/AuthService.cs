cc9c468222322f64d24d35cef3713004401eb7fd
saijaku0 <minerking112256@gmail.com>
2026-04-02 16:24:44 +0200

    fix: update register handler

--- src/Sneakers.Shop.Backend.Application/Auth/Command/AuthService.cs
+++ src/Sneakers.Shop.Backend.Application/Auth/Command/AuthService.cs
@@ -8,24 +8,18 @@ using Sneakers.Shop.Backend.Domain.Entities;
 namespace Sneakers.Shop.Backend.Application.Auth.Command
 {
     public class AuthService(
+        IUserProfileRepository userProfileRepository,
         IIdentityService identity, 
         IJwtService jwtService, 
         IRefreshTokenRepository refreshToken,
         IUnitOfWork unitOfWork) : IAuthService
     {
+        private readonly IUserProfileRepository _userProfileRepository = userProfileRepository;
         private readonly IIdentityService _identity = identity;
         private readonly IJwtService _jwtService = jwtService;
         private readonly IRefreshTokenRepository _refreshToken = refreshToken;
         private readonly IUnitOfWork _unitOfWork = unitOfWork;
 
-        /// <summary>
-        /// Asynchronously registers a new user with the specified registration details.    
-        /// </summary>
-        /// <param name="request">The registration information for the new user. Cannot be null.</param>
-        /// <param name="ct">A cancellation token that can be used to cancel the registration operation.</param>
-        /// <returns>A task that represents the asynchronous operation. The task result contains an AuthResponse with the outcome
-        /// of the registration.</returns>
-        /// <exception cref="InvalidOperationException">Thrown if a user with the specified email address already exists.</exception>
         public async Task<AuthResponse> RegisterAsync(
             RegisterRequest request, 
             CancellationToken ct = default)
@@ -38,18 +32,24 @@ namespace Sneakers.Shop.Backend.Application.Auth.Command
 
             await _identity.AssignRole(userId, UserRole.Customer, ct);
 
+            var userProfile = new UserProfile
+            (
+                userId,
+                request.Name,
+                request.Lastname,
+                request.PhoneNumber,
+                request.Email
+            );
+
+            await _userProfileRepository.AddAsync(userProfile, ct);
+
+            if(request.DefaultShippingAddress != null)
+                userProfile.UpdateDefaultAddress(request.DefaultShippingAddress);
+
+            await _unitOfWork.SaveChangesAsync(ct);
             return await GenerateNewPairToken(userId, ct);
         }
 
-        /// <summary>
-        /// Authenticates a user asynchronously using the provided login credentials and returns an authentication
-        /// response containing access tokens.
-        /// </summary>
-        /// <param name="request">The login request containing the user's email and password. Cannot be null.</param>
-        /// <param name="ct">A cancellation token that can be used to cancel the login operation.</param>
-        /// <returns>A task that represents the asynchronous operation. The task result contains an authentication response with
-        /// access and refresh tokens for the authenticated user.</returns>
-        /// <exception cref="InvalidOperationException">Thrown if the user is not found or if the login credentials are invalid.</exception>
         public async Task<AuthResponse> LoginAsync(
             LoginRequest request,
             CancellationToken ct = default)
@@ -63,16 +63,6 @@ namespace Sneakers.Shop.Backend.Application.Auth.Command
             return await GenerateNewPairToken(userId, ct);
         }
 
-        /// <summary>
-        /// Generates a new authentication token pair using the specified refresh token.
-        /// </summary>
-        /// <remarks>The method revokes the provided refresh token and issues a new token pair. The old
-        /// token cannot be reused after this operation.</remarks>
-        /// <param name="refreshToken">The refresh token used to request a new authentication token pair. Cannot be null or invalid.</param>
-        /// <param name="ct">A cancellation token that can be used to cancel the operation.</param>
-        /// <returns>A task that represents the asynchronous operation. The task result contains an <see cref="AuthResponse"/>
-        /// with the new authentication token pair.</returns>
-        /// <exception cref="InvalidOperationException">Thrown if the specified refresh token is null, not found, or is not valid.</exception>
         public async Task<AuthResponse> RefreshTokenAsync
             (string refreshToken, 
             CancellationToken ct = default)
@@ -88,15 +78,6 @@ namespace Sneakers.Shop.Backend.Application.Auth.Command
             return await GenerateNewPairToken(getUserToken.UserId, ct);
         }
 
-        /// <summary>
-        /// Generates a new pair of access and refresh tokens for the specified user.
-        /// </summary>
-        /// <remarks>The generated refresh token is persisted and associated with the specified user.
-        /// Existing refresh tokens are not revoked by this method.</remarks>
-        /// <param name="userId">The unique identifier of the user for whom the tokens are generated.</param>
-        /// <param name="ct">A cancellation token that can be used to cancel the asynchronous operation.</param>
-        /// <returns>A task that represents the asynchronous operation. The task result contains an AuthResponse with the newly
-        /// generated access and refresh tokens.</returns>
         private async Task<AuthResponse> GenerateNewPairToken(Guid userId, CancellationToken ct)
         {
             var tokenData = await _identity.GetTokenGenerationDataAsync(userId, ct);
