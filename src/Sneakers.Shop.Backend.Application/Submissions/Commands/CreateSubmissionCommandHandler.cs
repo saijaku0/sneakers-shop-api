0fa69dc18afa1d3bb95ebd48d6e2bfe21dbbcbdd
saijaku0 <minerking112256@gmail.com>
2026-04-02 12:47:00 +0200

    feat: add create submission for drop role

---
+++ src/Sneakers.Shop.Backend.Application/Submissions/Commands/CreateSubmissionCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using Sneakers.Shop.Backend.Domain.Entities;
+using Sneakers.Shop.Backend.Domain.Repositories;
+
+namespace Sneakers.Shop.Backend.Application.Submissions.Commands
+{
+    public class CreateSubmissionCommandHandler(
+        IUserProfileRepository userProfileRepository,
+        IBrandRepository brandRepository,
+        IProductSubmissionRepository productRepository,
+        IUnitOfWork unitOfWork)
+        : IRequestHandler<CreateSubmissionCommand, Guid>
+    {
+        private readonly IUserProfileRepository _userProfileRepository = userProfileRepository;
+        private readonly IBrandRepository _brandRepository = brandRepository;
+        private readonly IProductSubmissionRepository _productRepository = productRepository;
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+        public async Task<Guid> Handle(
+            CreateSubmissionCommand request, 
+            CancellationToken cancellationToken)
+        { 
+            var brand = await _brandRepository.GetByIdAsync(request.BrandId, cancellationToken)
+                ?? throw new ArgumentException($"Brand with ID {request.BrandId} does not exist.");
+
+            var isUserFlagged = await _userProfileRepository.IsUserFlaggedAsync(request.DropId, cancellationToken);
+            if (isUserFlagged)
+                throw new ArgumentException($"User profile for brand owner with ID {request.DropId} is flagged.");
+
+            var submission = new ProductSubmission
+            (
+                request.DropId,
+                request.BrandId,
+                request.TargetAudience,
+                request.ProductName,
+                request.Model,
+                request.Description,
+                request.BasePrice
+            );
+            await _productRepository.AddAsync(submission, cancellationToken);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+            return submission.Id;
+        }
+    }
+}
