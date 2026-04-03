796c0b1bb519b34f04b3fb265fdd369ae4cab5aa
saijaku0 <minerking112256@gmail.com>
2026-04-03 11:38:33 +0200

    feat: add event publisher

---
+++ src/Sneakers.Shop.Backend.Infrastructure/Events/DomainEventPublisher.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Sneakers.Shop.Backend.Application.Common;
+using Sneakers.Shop.Backend.Application.Common.Interfaces;
+using Sneakers.Shop.Backend.Domain.Events;
+
+namespace Sneakers.Shop.Backend.Infrastructure.Events
+{
+    public class DomainEventPublisher(IMediator mediator) : IDomainEventPublisher
+    {
+        private readonly IMediator _mediator = mediator;
+        public async Task PublishAsync(IDomainEvent domainEvent, CancellationToken ct = default)
+        {
+            var notificationType = typeof(DomainEventNotification<>).MakeGenericType(domainEvent.GetType());
+            var notification = Activator.CreateInstance(notificationType, domainEvent);
+            await _mediator.Publish(notification!, ct);
+        }
+    }
+}
