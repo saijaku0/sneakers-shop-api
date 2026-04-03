554c7a3838271685f39fedcfff7edda32050a944
saijaku0 <minerking112256@gmail.com>
2026-04-03 10:53:19 +0200

    feat: add user create event record

---
+++ src/Sneakers.Shop.Backend.Domain/Events/UserCreatedEvent.cs
@@ -0,0 +1,12 @@
+﻿using Sneakers.Shop.Backend.Domain.ValueObjects;
+
+namespace Sneakers.Shop.Backend.Domain.Events
+{
+    public record UserCreatedEvent(
+        Guid UserId,
+        string Name,
+        string Lastname,
+        string PhoneNumber,
+        string Email,
+        Address? DefaultShippingAddress);
+}
