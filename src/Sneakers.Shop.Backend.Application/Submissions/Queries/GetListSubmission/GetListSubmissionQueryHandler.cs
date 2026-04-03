df93085931288d9d8ab7e894eeaf21128be4cc4b
saijaku0 <minerking112256@gmail.com>
2026-04-03 13:38:03 +0200

    feat: add command to get list of submissions

--- src/Sneakers.Shop.Backend.Application/Submissions/Queries/GetListSubmission/GetListSubmissionQueryHandler.cs
+++ src/Sneakers.Shop.Backend.Application/Submissions/Queries/GetListSubmission/GetListSubmissionQueryHandler.cs
@@ -1,6 +1,42 @@
-﻿namespace Sneakers.Shop.Backend.Application.Submissions.Queries.GetListSubmission
+﻿using MediatR;
+using Sneakers.Shop.Backend.Application.Submissions.DTOs;
+using Sneakers.Shop.Backend.Application.Submissions.Queries.Response;
+using Sneakers.Shop.Backend.Domain.Exceptions;
+using Sneakers.Shop.Backend.Domain.Repositories;
+
+namespace Sneakers.Shop.Backend.Application.Submissions.Queries.GetListSubmission
 {
-    public class GetListSubmissionQueryHandler
+    public class GetListSubmissionQueryHandler(
+        IProductSubmissionRepository productRepository) 
+        : IRequestHandler<GetMySubmissionsQuery, GetSubmissionsResponse>
     {
+        private readonly IProductSubmissionRepository _productRepository = productRepository;
+        public async Task<GetSubmissionsResponse> Handle(
+            GetMySubmissionsQuery request, 
+            CancellationToken cancellationToken)
+        {
+            var submissions = await _productRepository.GetByDropperIdAsync(
+                request.DropId, 
+                request.Page, 
+                request.PageSize, 
+                cancellationToken);
+
+            var getTotalCount = await _productRepository.GetTotalCountByDropperIdAsync(
+                request.DropId, 
+                cancellationToken);
+            
+            var submissionsDtos = submissions.Select(s => new GetSubmissionDto
+            (
+                Id: s.Id,
+                ProductName: s.ProductName,
+                Brand: s.SneakersBrand?.BrandName ?? "Unknown",
+                Price: s.BasePrice,
+                Status: s.Status.ToString(),
+                CreatedAt: s.CreatedAt,
+                RejectionReason: s.RejectionReason
+            )).ToList();
+
+            return new GetSubmissionsResponse(submissionsDtos, getTotalCount);
+        }
     }
 }
