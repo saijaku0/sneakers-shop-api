106e5898139129b6a896c30940417b1cdfce9243
saijaku0 <minerking112256@gmail.com>
2026-04-05 11:37:03 +0200

    feat: add cancel submission command

---
+++ src/Sneakers.Shop.Backend.Application/Submissions/Commands/CancelSubmission/CancelSubmissionCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Sneakers.Shop.Backend.Application.Submissions.Commands.CancelSubmission
+{
+    public record CancelSubmissionCommand(
+        Guid SubmissionId,
+        Guid DropId) : IRequest<Unit>;
+}
---
+++ src/Sneakers.Shop.Backend.Application/Submissions/Commands/CancelSubmission/CancelSubmissionCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sneakers.Shop.Backend.Application.Submissions.Commands.CancelSubmission
+{
+    internal class CancelSubmissionCommandHandler
+    {
+    }
+}
