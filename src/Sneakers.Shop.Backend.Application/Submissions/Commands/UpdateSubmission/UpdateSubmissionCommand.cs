b0a634b2f194b290ae9c5c968a0f040cb0bebe40
saijaku0 <minerking112256@gmail.com>
2026-04-06 19:38:37 +0200

    feat: add command handler to update submissions

---
+++ src/Sneakers.Shop.Backend.Application/Submissions/Commands/UpdateSubmission/UpdateSubmissionCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Sneakers.Shop.Backend.Application.Submissions.DTOs;
+
+namespace Sneakers.Shop.Backend.Application.Submissions.Commands.UpdateSubmission
+{
+    public record UpdateSubmissionCommand(
+        Guid DropId,
+        Guid SubmissionId,
+        UpdateSubmissionRequest Payload
+        ) : IRequest<Unit>;
+}
---
+++ src/Sneakers.Shop.Backend.Application/Submissions/Commands/UpdateSubmission/UpdateSubmissionValidation.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+
+namespace Sneakers.Shop.Backend.Application.Submissions.Commands.UpdateSubmission
+{
+    public class UpdateSubmissionValidation : AbstractValidator<UpdateSubmissionCommand>
+    {
+        public UpdateSubmissionValidation() 
+        {
+            RuleFor(x => x.DropId)
+                .NotEmpty().WithMessage("User ID is required.");
+            RuleFor(x => x.Payload.BrandId)
+                .NotEmpty().WithMessage("Brand ID is required.");
+            RuleFor(x => x.Payload.ProductName)
+                .NotEmpty().WithMessage("Product name is required.");
+            RuleFor(x => x.Payload.Model)
+                .NotEmpty().WithMessage("Model is required.");
+            RuleFor(x => x.Payload.Description)
+                .NotEmpty().WithMessage("Description is required.")
+                .MinimumLength(10).WithMessage("Description must be at least 10 characters.")
+                .MaximumLength(1000).WithMessage("Description cannot exceed 1000 characters.");
+            RuleFor(x => x.Payload.BasePrice)
+                .NotEmpty()
+                .GreaterThanOrEqualTo(1)
+                .WithMessage("Base price cannot be negative.");
+            RuleFor(x => x.Payload.TargetAudience)
+                .IsInEnum()
+                .WithMessage("Invalid target audience.");
+        }
+    }
+}
