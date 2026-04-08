59b934262168429585bd977fbbcfaf6a85c8b896
saijaku0 <minerking112256@gmail.com>
2026-04-08 08:36:30 +0200

    feat: add get submission by id

---
+++ src/Sneakers.Shop.Backend.Application/Submissions/Queries/GetSubmissionById/GetSubmissionByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Sneakers.Shop.Backend.Application.Submissions.DTOs;
+
+namespace Sneakers.Shop.Backend.Application.Submissions.Queries.GetSubmissionById
+{
+    public record GetSubmissionByIdQuery(
+        Guid DropId,
+        Guid SubmissionId) : IRequest<GetSubmissionDto>;
+}
