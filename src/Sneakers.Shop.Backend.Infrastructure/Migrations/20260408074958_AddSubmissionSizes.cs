b5d1ee6f653dd2bc6c6c4c96b0b7609aca04927e
saijaku0 <minerking112256@gmail.com>
2026-04-08 09:50:35 +0200

    feat: add migration

---
+++ src/Sneakers.Shop.Backend.Infrastructure/Migrations/20260408074958_AddSubmissionSizes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Sneakers.Shop.Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSubmissionSizes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SubmissionSize",
+                columns: table => new
+                {
+                    ProductSubmissionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    SizeInCm = table.Column<decimal>(type: "numeric(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubmissionSize", x => new { x.ProductSubmissionId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_SubmissionSize_ProductSubmissions_ProductSubmissionId",
+                        column: x => x.ProductSubmissionId,
+                        principalTable: "ProductSubmissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SubmissionSize");
+        }
+    }
+}
