a7cdc9e66f12eb6cb2dbafa35da3d5c542393883
saijaku0 <minerking112256@gmail.com>
2026-04-09 22:04:50 +0200

    feat: add migrations

--- src/Sneakers.Shop.Backend.Infrastructure/Migrations/20260409200427_AddDropperNavigationToSubmission.Designer.cs
+++ src/Sneakers.Shop.Backend.Infrastructure/Migrations/20260409200427_AddDropperNavigationToSubmission.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Sneakers.Shop.Backend.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using Sneakers.Shop.Backend.Infrastructure.Persistence;
 namespace Sneakers.Shop.Backend.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260409200427_AddDropperNavigationToSubmission")]
+    partial class AddDropperNavigationToSubmission
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Sneakers.Shop.Backend.Infrastructure/Migrations/20260409200427_AddDropperNavigationToSubmission.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sneakers.Shop.Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDropperNavigationToSubmission : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductSubmissions_UserProfiles_DropId",
+                table: "ProductSubmissions",
+                column: "DropId",
+                principalTable: "UserProfiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductSubmissions_UserProfiles_DropId",
+                table: "ProductSubmissions");
+        }
+    }
+}
