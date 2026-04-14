f4d1de45798bc9582bf0cf00af0e986e278cf93e
Vadym <minerking112256@gmail.com>
2026-04-14 18:48:54 +0200

    chore: add list of domain events in entity

--- src/Sneakers.Shop.Backend.Domain/Abstractions/Entity.cs
+++ src/Sneakers.Shop.Backend.Domain/Abstractions/Entity.cs
@@ -1,8 +1,12 @@
-﻿namespace Sneakers.Shop.Backend.Domain.Abstractions
+﻿using Sneakers.Shop.Backend.Domain.Events;
+
+namespace Sneakers.Shop.Backend.Domain.Abstractions
 {
     public abstract class Entity : IEquatable<Entity>
     {
         public Guid Id { get; protected set; }
+        public IReadOnlyList<IDomainEvent> Events => [.. _events];
+        private readonly List<IDomainEvent> _events = [];
 
         protected Entity(Guid id)
         {
@@ -12,6 +16,9 @@
         }
 
         protected Entity() { }
+
+        public void AddEvent(IDomainEvent domainEvent) => _events.Add(domainEvent);
+        public void ClearEvents() => _events.Clear();
 
         public bool Equals(Entity? other) =>
             other != null && GetType() == other.GetType() && Id.Equals(other.Id);
