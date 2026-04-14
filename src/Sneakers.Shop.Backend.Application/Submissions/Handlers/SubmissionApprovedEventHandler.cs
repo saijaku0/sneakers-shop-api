6ab9f38bc6b7b52e22e108bd01877a1bcd429f7c
Vadym <minerking112256@gmail.com>
2026-04-14 19:04:51 +0200

    feat: add event to create product

---
+++ src/Sneakers.Shop.Backend.Application/Submissions/Handlers/SubmissionApprovedEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Sneakers.Shop.Backend.Domain.Entities;
+using Sneakers.Shop.Backend.Domain.Events;
+using Sneakers.Shop.Backend.Domain.Repositories;
+
+namespace Sneakers.Shop.Backend.Application.Submissions.Handlers
+{
+    public class SubmissionApprovedEventHandler(
+        IProductSubmissionRepository submissionRepository,
+        IProductRepository productRepository) 
+        : INotificationHandler<SubmissionApprovedEvent>
+    {
+        private readonly IProductSubmissionRepository _submissionRepository = submissionRepository;
+        private readonly IProductRepository _productRepository = productRepository;
+        public async Task Handle(SubmissionApprovedEvent notification, CancellationToken cancellationToken)
+        {
+            var submission = await _submissionRepository.GetByIdAsync(notification.SubmissionId, cancellationToken);
+            if (submission is null)
+                return;
+            var newProduct = new Product(
+                submission.BrandId,
+                submission.TargetAudience,
+                submission.ProductName,
+                submission.Model,
+                submission.Description,
+                submission.BasePrice);
+            await _productRepository.AddAsync(newProduct, cancellationToken);
+        }
+    }
+}
