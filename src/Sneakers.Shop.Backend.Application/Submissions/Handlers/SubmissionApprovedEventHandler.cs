9fbac9d1d0132051e87dc526b3e4a57602cb9080
Vadym <minerking112256@gmail.com>
2026-04-14 19:24:53 +0200

    fix: submission event handler

--- src/Sneakers.Shop.Backend.Application/Submissions/Handlers/SubmissionApprovedEventHandler.cs
+++ src/Sneakers.Shop.Backend.Application/Submissions/Handlers/SubmissionApprovedEventHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Sneakers.Shop.Backend.Application.Common;
 using Sneakers.Shop.Backend.Domain.Entities;
 using Sneakers.Shop.Backend.Domain.Events;
 using Sneakers.Shop.Backend.Domain.Repositories;
@@ -7,14 +8,21 @@ namespace Sneakers.Shop.Backend.Application.Submissions.Handlers
 {
     public class SubmissionApprovedEventHandler(
         IProductSubmissionRepository submissionRepository,
-        IProductRepository productRepository) 
-        : INotificationHandler<SubmissionApprovedEvent>
+        IProductRepository productRepository,
+        IUnitOfWork unitOfWork) 
+        : INotificationHandler<DomainEventNotification<SubmissionApprovedEvent>>
     {
         private readonly IProductSubmissionRepository _submissionRepository = submissionRepository;
         private readonly IProductRepository _productRepository = productRepository;
-        public async Task Handle(SubmissionApprovedEvent notification, CancellationToken cancellationToken)
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+        public async Task Handle(
+            DomainEventNotification<SubmissionApprovedEvent> notification, 
+            CancellationToken cancellationToken)
         {
-            var submission = await _submissionRepository.GetByIdAsync(notification.SubmissionId, cancellationToken);
+            var submission = await _submissionRepository.GetByIdAsync(notification
+                .DomainEvent
+                .SubmissionId, 
+                cancellationToken);
             if (submission is null)
                 return;
             var newProduct = new Product(
@@ -24,7 +32,9 @@ namespace Sneakers.Shop.Backend.Application.Submissions.Handlers
                 submission.Model,
                 submission.Description,
                 submission.BasePrice);
+            newProduct.UpdateProductPhoto([.. submission.ImagesUrls]);
             await _productRepository.AddAsync(newProduct, cancellationToken);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
         }
     }
 }
