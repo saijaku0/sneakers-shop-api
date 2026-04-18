a3f8416dd19203e6a9bcbcb677802659a46f7eb3
Vadym <minerking112256@gmail.com>
2026-04-18 14:11:09 +0200

    feat: add update and cancel reservation

---
+++ src/Sneakers.Shop.Backend.Application/Reservations/Command/CancelReservation/CancelReservationCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Sneakers.Shop.Backend.Domain.Common;
+
+namespace Sneakers.Shop.Backend.Application.Reservations.Command.CancelReservation
+{
+    public record CancelReservationCommand(
+        Guid ReservationId,
+        Guid UserId) : IRequest<Result>;
+}
---
+++ src/Sneakers.Shop.Backend.Application/Reservations/Command/UpdateReservationQuantity/UpdateReservationQuantityCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Sneakers.Shop.Backend.Domain.Common;
+
+namespace Sneakers.Shop.Backend.Application.Reservations.Command.UpdateReservationQuantity
+{
+    public record UpdateReservationQuantityCommand(
+        Guid ReservationId,
+        Guid UserId,
+        int NewQuantity) : IRequest<Result>;
+}
