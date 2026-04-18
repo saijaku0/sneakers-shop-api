3b1c4a06de1ff289b38f809541fde7c20832c521
Vadym <minerking112256@gmail.com>
2026-04-18 13:55:02 +0200

    feat: add create reservation

---
+++ src/Sneakers.Shop.Backend.Application/Reservations/Command/CreateReservation/CreateReservationCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Sneakers.Shop.Backend.Domain.Common;
+
+namespace Sneakers.Shop.Backend.Application.Reservations.Command.CreateReservation
+{
+    public record CreateReservationCommand(
+        Guid WarehouseItemId,
+        Guid UserId,
+        int Quantity) : IRequest<Result<Guid>>;
+}
---
+++ src/Sneakers.Shop.Backend.Application/Reservations/Command/CreateReservation/CreateReservationHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using Sneakers.Shop.Backend.Domain.Common;
+using Sneakers.Shop.Backend.Domain.Entities;
+using Sneakers.Shop.Backend.Domain.Repositories;
+
+namespace Sneakers.Shop.Backend.Application.Reservations.Command.CreateReservation
+{
+    public class CreateReservationHandler(
+        IReservationRepository reservationRepository,
+        IGenericRepository<WarehouseItem> warehouseItemRepository,
+        IUnitOfWork unitOfWork) 
+        : IRequestHandler<CreateReservationCommand, Result<Guid>>
+    {
+        private readonly IReservationRepository _reservationRepository = reservationRepository;
+        private readonly IGenericRepository<WarehouseItem> _warehouseItemRepository = warehouseItemRepository;
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+        public async Task<Result<Guid>> Handle(CreateReservationCommand request, CancellationToken ct)
+        {
+            var warehouseItem = await _warehouseItemRepository.GetByIdAsync(request.WarehouseItemId, ct);
+            if (warehouseItem == null) 
+                return Result<Guid>.Failure(Error.NotFound("Warehouse item not found."));
+
+            var reservedCount = await _reservationRepository.GetReservedCountAsync(request.WarehouseItemId, ct);
+            var available = warehouseItem.Quantity - reservedCount;
+
+            if (request.Quantity > available)
+                return Result<Guid>.Failure(Error.Conflict($"Not enough stock. Available: {available}"));
+
+            var reservation = new Reservation
+            (
+                request.WarehouseItemId,
+                request.UserId,
+                DateTimeOffset.UtcNow.AddMinutes(120),
+                request.Quantity
+            );
+            await _reservationRepository.AddAsync(reservation, ct);
+            await _unitOfWork.SaveChangesAsync(ct);
+
+            return Result<Guid>.Success(reservation.Id);
+        }
+    }
+}
