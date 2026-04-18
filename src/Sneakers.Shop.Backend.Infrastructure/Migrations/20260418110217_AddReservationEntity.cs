3acfb41ab67aaf29c3316b7e611c1a3d2ea1dd1a
Vadym <minerking112256@gmail.com>
2026-04-18 13:03:31 +0200

    chore: make migration

---
+++ src/Sneakers.Shop.Backend.Infrastructure/Migrations/20260418110217_AddReservationEntity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sneakers.Shop.Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReservationEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Reserved",
+                table: "WarehouseItems");
+
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    WarehouseItemId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    ExpiresAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_ExpiresAt",
+                table: "Reservations",
+                column: "ExpiresAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_WarehouseItemId_Status",
+                table: "Reservations",
+                columns: new[] { "WarehouseItemId", "Status" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reservations");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Reserved",
+                table: "WarehouseItems",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
