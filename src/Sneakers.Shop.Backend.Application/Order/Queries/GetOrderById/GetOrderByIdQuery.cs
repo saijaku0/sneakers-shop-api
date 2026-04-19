8f621f7f668875ee043b1eb6f29956d4c324e22c
Vadym <minerking112256@gmail.com>
2026-04-19 15:43:35 +0200

    feat: add order controller

---
+++ src/Sneakers.Shop.Backend.Application/Order/Queries/GetOrderById/GetOrderByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Sneakers.Shop.Backend.Application.Order.DTOs;
+using Sneakers.Shop.Backend.Domain.Common;
+
+namespace Sneakers.Shop.Backend.Application.Order.Queries.GetOrderById
+{
+    public record GetOrderByIdQuery(Guid OrderId, Guid UserId)
+        : IRequest<Result<OrderDetailsDto>>;
+}
